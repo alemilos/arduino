47452dc6e73df8f7c10daa5954f36ed020603ba3
Alessandro Milos <alessandro.milos@raxarsystems.com>
2024-12-02 22:45:20 +0100

    just potentiometer

---
+++ capacitor-charge-discharge/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+// put function declarations here:
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  // print out the value you read:
+  Serial.println(voltage);
+}
